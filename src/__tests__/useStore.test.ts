136048fc24a7206aee37c9e0165b5b3b03d9a011
futantan <luckytantanfu@gmail.com>
2019-11-11 20:51:59 +0800

    test: add test for useStore

--- src/__tests__/useStore.test.ts
+++ src/__tests__/useStore.test.ts
@@ -1,3 +1,28 @@
-test('canary test', () => {
-  expect(1).toBe(1)
+import { createEvent, createStore } from 'stadux'
+import { act, renderHook } from '@testing-library/react-hooks'
+import { useStore } from '../index'
+
+test('should be able to watch store change', () => {
+  const increment = createEvent('increment')
+  const decrement = createEvent('decrement')
+  const resetCounter = createEvent('reset counter')
+  const counter = createStore(0)
+    .on(increment, state => state + 1)
+    .on(decrement, state => state - 1)
+    .reset(resetCounter)
+
+  const { result } = renderHook(() => useStore(counter))
+  expect(result.current).toBe(0)
+
+  act(() => increment())
+  expect(result.current).toBe(1)
+
+  act(() => increment())
+  expect(result.current).toBe(2)
+
+  act(() => decrement())
+  expect(result.current).toBe(1)
+
+  act(() => resetCounter())
+  expect(result.current).toBe(0)
 })
